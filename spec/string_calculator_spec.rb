f368c0ca7232e25c8a662bac1fcec77f1e31ee18
Rafael George <george.rafael@gmail.com>
2013-11-21 18:28:56 -0400

    Add failing example for returning single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,6 +6,10 @@ describe StringCalculator do
     it 'returns 0 with empty' do 
       expect("".extend(StringCalculator).add).to eql 0
     end
+
+    it 'returns single number' do 
+      expect("1".extend(StringCalculator).add).to eql 1
+    end
   end
 
 end
