e607c1183190ac6d52830f36e4531fd227a4e188
Rafael George <george.rafael@gmail.com>
2013-11-21 22:22:10 -0400

    Add a refactor to clean up the coupling between String/Numbers/Delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,38 @@ require "string_calculator/version"
 
 module StringCalculator
   def add 
-    self.to_i
+    return 0 if empty?
+    Numbers.new(self, Delimiter.for(self)).sum
   end
+
+  class Numbers 
+    def initialize(nums, delimiter)
+      @delimiter = delimiter
+      @values = nums.split(delimiter).map(&:to_i)
+    end
+
+    def sum
+      @values.reduce(&:+)
+    end
+  end
+
+  class Delimiter 
+    def self.for(values)
+      new(values).delimiter
+    end
+
+    def initialize(values)
+      @values = values
+      freeze
+    end
+
+    def delimiter
+      if @values.start_with? '//'
+        @values[2,1]
+      else 
+        /,|\n/
+      end
+    end
+  end
+
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,4 +12,30 @@ describe StringCalculator do
     end
   end
 
+  context 'more than one number' do 
+    it 'returns the sum of two numbers' do 
+      expect("1,2".extend(StringCalculator).add).to eql 3
+    end
+
+    it 'returns the sum of an unknown amount of numbers' do 
+      expect("1,2,3,4,5".extend(StringCalculator).add).to eql 15
+    end
+  end
+
+  context 'different delimeters' do 
+    it 'returns the sum of two numbers with newline as delimiter' do 
+      expect("1\n2".extend(StringCalculator).add).to eql 3
+    end
+
+    it 'returns the sum of an unknown amount of numbers with mixed delimeters' do 
+      expect("1,2,3\n4,5".extend(StringCalculator).add).to eql 15
+    end
+  end
+
+  context 'changing the delimeter' do 
+    it 'returns the sum of two numbers with a different delimeter' do 
+      expect("//;\n1;2".extend(StringCalculator).add).to eql 3
+    end
+  end
+
 end
